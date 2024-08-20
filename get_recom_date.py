b2717b1f9e445cd8818befcc19f157864ed35823
leviafan14 <14leviafan88@gmail.com>
2024-08-20 22:25:06 +0500

    add get recom-ion date

---
+++ get_recom_date.py
@@ -0,0 +1,45 @@
+import csv
+from request_recomendation import send_get_request, url, access_token
+
+# Имя файла, в котором нужно искать записи
+file_name = r"C:\My_files\Anothers\Talisman\rawr1.csv"
+# Имя выходного файла c найденными записями Excel
+output_file_name = "output.xlsx"
+
+# Определение целевых значений для поиска
+target_status = "Бронь"
+target_total_amount = 0
+
+# Создание пустого списка для найденных записей
+found_records = []
+invalid_records = []
+invalid_rows_count = 0
+without_recomendation = []
+valid_rows_count = 0
+column = "Сделка"
+
+# Открытие файла и чтение его содержимого
+with open(file_name, "r", encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        # Проверка значений столбцов
+        if len(row[column]) > 0:
+            found_records.append({"FIO": row["ФИО"], "Deal_ID": row[column]})
+            valid_rows_count += 1
+
+# Вывод количества найденных записей
+print(f"\nКоличество записей:\n{len(found_records)}")
+
+deals_with_date = []
+deals_invalid_date = []
+invalid_users_count = 0
+if found_records:
+    for i in found_records:
+        inserted_at = send_get_request(url, i['Deal_ID'], access_token)
+        if "2024" in str(inserted_at):
+            deals_with_date.append({"FIO": i["FIO"], "Deal": i['Deal_ID'], 'Date': inserted_at})
+        else:
+            deals_invalid_date.append({"FIO": i["FIO"], "Deal": i['Deal_ID'], 'Date': inserted_at})
+            invalid_users_count += 1
+            print(invalid_users_count)
+print(deals_invalid_date, f"\n Количество невалидных записей: {invalid_users_count}")
--- request_recomendation.py
+++ request_recomendation.py
@@ -37,7 +37,7 @@ def send_get_request(url: str, raw_deal_id: str, access_token: str):
         recom = response.json()
         raw_inserted_at = recom['get_deal']['result']['values']['inserted_at']
         inserted_at = datetime.fromisoformat(raw_inserted_at).date()
-        print(inserted_at)
+        return inserted_at
     else:
         print(f"Request failed with status code: {response.status_code}")
         print(response.text)
@@ -47,5 +47,6 @@ url = "https://crm.talisman-online.ru/api/v1/batch"
 access_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpLmFzdGFzaG92IiwiZXhwIjoxNzI0MTY2OTkzfQ.QoyBfnPs3GTw39Y2H21l1zuvjkiITKSMk5aOZzSEjVT-7T2f540mmvBuGASrZ0VGwcjP9qtMBZ8oip0qoHitXffhU33tBGp7PHPaK6nHQzhywfR5RUmiTcrXXu9R_C4_L5vn9oPzMkbVNkXJhwbBNSZaZX7ThkgmhZ485XrJKh3YyiNAP-e89JA-URrPNQf8FmjKInXrYUSpSF6iLkzmwLqnlWECKGTdqHIbg_ysMbFWQe5Vg89Tjvr7CLlT6BzFpHXRQYFrSvsnq3VVvin0z5jEe3s5Puw4V2QHbRWbCLQA9dmout-_o4AmYoLnuMUPdIbL0ZLvSyz1GDaPZ9ZWriEeokoz6M4iu7iYSXHhuo0YWPyuit1ny2Vd9u1DA7JJgDRiu5q2dPSxjlXX_qWXYbT3RbOSBlqmeZm_cK57sScB_qD7dDNvvjiTYhy1z39gKCNUqjYcvQNLxZ6ICbnIE7FH07gSFAxeJdv5oQN10yQHdGFy_rGvFUk_9cyWkIF4ha5VursGsqU3GBl7RGaDWSGRBnAEWLaPVo6oZ_pbWIrEsKEDQaq4WCvGlDzoCy9GRW9vhWgSLoIMGqBYNTTVG6vLz29x3Q7yDAEXcfFqtMfHnSrz2qapG18mkDbBNWlfI4eysZlHL_s63Ic-IBzXyyqtNoBAIX8Z_2ylNKvdh0Y"
 
 
-# Вызываем функцию
-print(send_get_request(url, "431 759", access_token))
+if __name__ == "__main__":
+    # Вызываем функцию
+    print(send_get_request(url, "431 759", access_token))
